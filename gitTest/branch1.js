ab3b5002f428d64210fa88fb175c6a596cbf8110
jrholm <50438195+jrholm@users.noreply.github.com>
2019-05-08 23:19:55 -0700

    Add files via upload

---
+++ gitTest/branch1.js
@@ -0,0 +1,5 @@
+var str = "A Cool Video!";
+var result = str.link("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
+
+var number = 20;
---
+++ gitTest/branch2.js
@@ -0,0 +1,3 @@
+var str = "Actually a Rick Roll";
+
+var number = 20;
